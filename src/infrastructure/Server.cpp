64e481e53da4e8a9521f705f5bc10fc72c3651bc
root-kidik <rtkid.nik@gmail.com>
2025-02-04 17:51:41 +0700

    feat: simplify

--- src/infrastructure/Server.cpp
+++ src/infrastructure/Server.cpp
@@ -7,7 +7,6 @@
 
 #include <infrastructure/Server.hpp>
 #include <infrastructure/client/TcpSocketUserConnection.hpp>
-
 #include <infrastructure/utils/QtTimer.hpp>
 
 namespace rps::infrastructure
@@ -32,44 +31,20 @@ Server::Server() : m_memory_user_storage{m_uuid_generator}, m_memory_room_storag
             {
                 auto* client_socket = nextPendingConnection();
 
+                auto client_wrapper = std::make_shared<client::TcpSocketUserConnection>(client_socket);
+
+                connect(client_socket,
+                        &QTcpSocket::disconnected,
+                        this,
+                        [client_wrapper]() { client_wrapper->disconnect(); });
+
+                connect(client_socket, &QTcpSocket::disconnected, client_socket, &QTcpSocket::deleteLater);
+
                 connect(client_socket,
                         &QTcpSocket::readyRead,
                         this,
-                        [client_socket, this]()
-                        {
-                            std::istringstream iss{client_socket->readAll().toStdString()};
-
-                            protocol::entity::CommandRepresentation command_type;
-                            iss >> command_type;
-
-                            if (command_type < static_cast<protocol::entity::CommandRepresentation>(
-                                                   protocol::entity::server::ServerCommandType::Begin) +
-                                                   1 ||
-                                command_type > static_cast<protocol::entity::CommandRepresentation>(
-                                                   protocol::entity::server::ServerCommandType::End) -
-                                                   1)
-                                return;
-
-                            std::string data;
-                            std::getline(iss, data);
-
-                            if (!data.empty() && std::isspace(data.front()))
-                                data.erase(0, 1);
-
-                            auto client_wrapper = std::make_shared<client::TcpSocketUserConnection>(client_socket);
-                            connect(client_socket,
-                                    &QTcpSocket::disconnected,
-                                    this,
-                                    [client_wrapper]() { client_wrapper->disconnect(); });
-
-
-                            m_command_executor.execute_command(static_cast<protocol::entity::server::ServerCommandType>(
-                                                                   command_type),
-                                                               std::move(data),
-                                                               client_wrapper);
-                        });
-
-                connect(client_socket, &QTcpSocket::disconnected, client_socket, &QTcpSocket::deleteLater);
+                        [client_socket, client_wrapper, this]()
+                        { m_command_executor.execute_command(client_socket->readAll().toStdString(), client_wrapper); });
             });
 }
 
