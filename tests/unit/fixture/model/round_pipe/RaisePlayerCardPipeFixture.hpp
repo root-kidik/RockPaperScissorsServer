fd1d04e03d52283f54e4237d294bb7c853e3df3d
root-kidik <rtkid.nik@gmail.com>
2025-02-06 23:31:31 +0700

    feat: add more tests

---
+++ tests/unit/fixture/model/round_pipe/RaisePlayerCardPipeFixture.hpp
@@ -0,0 +1,21 @@
+#include <memory>
+#include <mock/client/ConnectionMock.hpp>
+#include <mock/util/TimerMock.hpp>
+
+#include <domain/model/round_pipe/RaisePlayerCardPipe.hpp>
+
+class RaisePlayerCardPipeFixture : public testing::Test
+{
+public:
+    RaisePlayerCardPipeFixture()
+    {
+        player.connection = connection;
+    }
+
+    std::shared_ptr<ConnectionMock>                connection = std::make_shared<ConnectionMock>();
+    domain::model::Room::Player                    player;
+    domain::entity::Uuid                           uuid;
+    domain::model::Room::RoundContext              context{player, uuid};
+    protocol::entity::CommandSender                command_sender;
+    domain::model::round_pipe::RaisePlayerCardPipe pipe{command_sender};
+};
---
+++ tests/unit/src/model/round_pipe/RaisePlayerCardPipeTest.cpp
@@ -0,0 +1,70 @@
+#include <fixture/model/round_pipe/RaisePlayerCardPipeFixture.hpp>
+
+#include <gtest/gtest.h>
+
+#include <RockPaperScissorsProtocol/entity/client/request/CardRaisedRequest.hpp>
+
+using testing::Return;
+
+TEST_F(RaisePlayerCardPipeFixture, run_raise_rock)
+{
+    auto raised_card = protocol::entity::Card::Rock;
+
+    player.nominated_card = raised_card;
+    player.cards.emplace_back(raised_card);
+
+    protocol::entity::client::CardRaisedRequest request;
+    request.card = raised_card;
+
+    EXPECT_CALL(*connection, send(protocol::util::serialize_request(std::move(request)))).WillOnce(Return());
+
+    pipe.run(context);
+
+    EXPECT_TRUE(player.cards.empty());
+
+    EXPECT_TRUE(context.is_rock_raised);
+    EXPECT_FALSE(context.is_paper_raised);
+    EXPECT_FALSE(context.is_scissors_raised);
+}
+
+TEST_F(RaisePlayerCardPipeFixture, run_raise_paper)
+{
+    auto raised_card = protocol::entity::Card::Paper;
+
+    player.nominated_card = raised_card;
+    player.cards.emplace_back(raised_card);
+
+    protocol::entity::client::CardRaisedRequest request;
+    request.card = raised_card;
+
+    EXPECT_CALL(*connection, send(protocol::util::serialize_request(std::move(request)))).WillOnce(Return());
+
+    pipe.run(context);
+
+    EXPECT_TRUE(player.cards.empty());
+
+    EXPECT_FALSE(context.is_rock_raised);
+    EXPECT_TRUE(context.is_paper_raised);
+    EXPECT_FALSE(context.is_scissors_raised);
+}
+
+TEST_F(RaisePlayerCardPipeFixture, run_raise_scissors)
+{
+    auto raised_card = protocol::entity::Card::Scissors;
+
+    player.nominated_card = raised_card;
+    player.cards.emplace_back(raised_card);
+
+    protocol::entity::client::CardRaisedRequest request;
+    request.card = raised_card;
+
+    EXPECT_CALL(*connection, send(protocol::util::serialize_request(std::move(request)))).WillOnce(Return());
+
+    pipe.run(context);
+
+    EXPECT_TRUE(player.cards.empty());
+
+    EXPECT_FALSE(context.is_rock_raised);
+    EXPECT_FALSE(context.is_paper_raised);
+    EXPECT_TRUE(context.is_scissors_raised);
+}
